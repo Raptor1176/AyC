e0151d3e9a8fc72e86116b145734b784075be661
Rodrigo Díaz <rodrigomdiaz@live.com>
2019-06-13 20:24:47 -0300

    terminado conj disj con heuristicas

--- src/ConjuntosDisjuntos.java
+++ src/ConjuntosDisjuntos.java
@@ -2,20 +2,18 @@ import estructuras.NodoCD;
 
 
 public class ConjuntosDisjuntos<E> {
-
-	private int tamanio;
+	
 	// Necesito tambien un arreglo con los elementos para retornarlos en el findset
 	private E[] elementos;
-	private int[] raices;
+	private int[] padres;
 	private int[] rangos;
 	private boolean heuristicas;
 	
 	@SuppressWarnings("unchecked")
 	public ConjuntosDisjuntos(int tamanio, boolean usarHeuristicas) {		
 		
-		this.tamanio=tamanio;
 		this.heuristicas=usarHeuristicas;
-		this.raices= new int[tamanio];
+		this.padres= new int[tamanio];
 		this.rangos= new int[tamanio];
 		this.elementos=(E[]) new Object[tamanio];
 	}
@@ -24,34 +22,90 @@ public class ConjuntosDisjuntos<E> {
 	public void makeSet(E elem) {		
 		int index=elem.getID();
 		this.elementos[index]=elem;
-		this.raices[index]=index;
+		this.padres[index]=index;
 		this.rangos[index]=0;		
 	}
 	
 	public E findSet(E elem) {
+		// Obtengo el indice del elemento
 		int index=elem.getID();
-		if(this.raices[index]==index) {			
-			// Si el nodo tiene como padre a si mismo
-			// entonces el elemento identificador es el mismo nodo
+		
+		
+		if(this.padres[index]==index) {			
+			// Si el elemento tiene como padre a si mismo
+			// entonces el elemento identificador es el mismo elemento
 			return elem;
 		}
 		else {
-			// Si no llamo recursivamente
-			return findSet(this.elementos[this.raices[index]]);
+			// Si no llamo recursivamente para hallar al elemento representante
+			E representante = findSet(this.elementos[this.padres[index]]);
+			
+			if (this.heuristicas) {
+				/*
+				 * Heuristica de compresion de caminos
+				 */					
+				// Obtengo el indice del representante
+				int indexRepresentante= representante.getID();
+				
+				// Acorto el camino poniendo como padre al padre del representante (que es el mismo)
+				this.padres[index]=this.padres[indexRepresentante];				
+			}
+			
+			// Retorno el elemento representante
+			return representante;
 		}
-	}
+	}	
 	
 	public void union(E e1, E e2) {
+		// Obtengo los indices de los elementos
 		int index1= e1.getID();
-		int index2= e2.getID();		
-		if(this.raices[index1]==this.raices[index2]) {
+		int index2= e2.getID();
+		
+		// Obtengo los elementos padres (representantes) de los elementos		
+		int padreE1= this.padres[index1];
+		int padreE2= this.padres[index2];
+		
+		if(padreE1==padreE2) {
 			// Mismos padres, ambos pertenecen al mismo conjunto
 			// Entonces hay que hacer nada
 			return ;
 		}
 		else {
-			// Sino, seteo como raiz de e2 a la raiz de e1
-			this.raices[index2]=this.raices[index1];
+			
+			if (this.heuristicas) {				
+				/*
+				 * Heuristica de union por rangos
+				 */
+			
+				// Si el rango de e1 es menor que el rango de e2
+		        if (this.rangos[padreE1] < this.rangos[padreE2]) {	  
+		        	// Entonces muevo al conjunto que contiene a e1 
+		        	// debajo del conjunto que contiene e2
+		            this.padres[padreE1] = padreE2;
+				}
+		        // Sino, si el rango de e2 es menor que el de e1
+		        else if (this.rangos[padreE2] < this.rangos[padreE1]) {
+		        	// Entonces muevo al conjunto que contiene a e2 
+		        	// debajo del conjunto que contiene e1
+		            this.padres[padreE2] = padreE1; 
+		        }
+		        else {
+		        	// Si los rangos son iguales entonces
+		        	// muevo uno conjunto debajo de otro, no importa cual
+		            this.padres[padreE2] = padreE1; 
+		  
+		            // Luego incremento el rango al cual le puse al otro conjunto debajo
+		            this.rangos[padreE1] = this.rangos[padreE1] + 1; 
+		        }	        
+			}
+			else {
+				/*
+				 * Sin heuristicas
+				 */
+				
+				// Sino, seteo como padre de e2 al padre de e1
+				this.padres[index2]=this.padres[index1];
+			}
 		}
 	}
 	
