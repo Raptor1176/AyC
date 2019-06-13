bab64e37a97d04c1e1b01118bb58a255bdd938fc
Rodrigo Díaz <rodrigomdiaz@live.com>
2019-06-13 18:08:27 -0300

    asd

--- src/estructuras/Lista.java
+++ src/estructuras/Lista.java
@@ -4,56 +4,56 @@ import estructuras.ILista;
 public class Lista<E> implements ILista<E>{
 
 	private class Nodo<N>{
-		N e;
-		Nodo<N> sig;
+		N elemento;
+		Nodo<N> siguiente;
 		
-		private Nodo(N e){
-			this.e=e;
-			sig=null;
+		private Nodo(N elem){
+			this.elemento=elem;
+			this.siguiente=null;
 		}
 	}
 	
-	private Nodo<E> p;	
-	private Nodo<E> u;	
-	private int t;
+	private Nodo<E> primero;	
+	private Nodo<E> ultimo;	
+	private int tamaño;
 	
 	public Lista(){
-		p=null;
-		u=null;
-		t=0;
+		this.primero=null;
+		this.ultimo=null;
+		this.tamaño=0;
 	}
 	
 	public void add(E e){
-		if(p!=null){
-			Nodo<E> aux= new Nodo<E>(e);
-			u.sig=aux;
-			u=u.sig;
+		if(this.primero!=null){
+			Nodo<E> nuevo= new Nodo<E>(e);
+			this.ultimo.siguiente=nuevo;
+			this.ultimo=this.ultimo.siguiente;
 		}else{
-			p= new Nodo<E>(e);
-			u=p;
+			this.primero= new Nodo<E>(e);
+			this.ultimo=primero;
 		}
-		t++;
+		tamaño++;
 	}
 	
 	
 	public int size(){
-		return t;
-	}
-	@Override
-	public E first() {		
-		return p.e;
+		return tamaño;
 	}
 
-	@Override
+	public E first() {		
+		return primero.elemento;
+	}
+
+
 	public E last() {		
-		return u.e;
+		return ultimo.elemento;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public E[] all() {
 		E[] arr= null;
-		if(t>0){			
+		if(tamaño>0){			
 			arr= (E[]) new Object[t];
 			Nodo<E> a=p;
 			int i=0;
