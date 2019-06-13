7095c050b4455e74bb26a20441831e17f594eca9
Raptor <ruben_dario2001ar@hotmail.com>
2019-06-13 18:16:42 -0300

    asdasd

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,16 @@
 import estructuras.ILista;
+import estructuras.Lista;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ILista l= new Lista();
+		ILista<Integer> l= new Lista<Integer>();
 		l.add(8);
 		l.add(5);
 		l.add(4);
 		
 		
-		int[] a= l.all();
+		Object[] a= l.all();
 		for(int i=0; i<a.length; i++)
 			System.out.print(a[i]+" ");
 		System.out.println();
